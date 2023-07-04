a9cbb36713c696f4071b84c3f6fab47d17fc2fa4
quydoan0316 <quydoan0316@gmail.com>
2023-07-04 20:45:09 +0700

    done

--- exercises/basic-of-rust/src/conditions.rs
+++ exercises/basic-of-rust/src/conditions.rs
@@ -4,15 +4,28 @@
 // - another function call
 // - additional variables
 pub fn bigger(a: i32, b: i32) -> i32 {
-    todo!()
+    if a > b{
+        a
+    }
+    else{
+        b
+    }
 }
 
 //Exercise 2
 // Input: Provide an arbitrary value of number
 // Check number is Positive or Negative or Zero
 // Output: &str
-fn check_number(number: u32) -> &'static str {
-    todo!()
+fn check_number(number: i32) -> &'static str {
+    if number == 0 {
+        "Zero"
+    }
+    else if number > 0 {
+        "Positive"
+    }
+    else{
+        "Negative"
+    }
 }
 
 // Exercise 3
@@ -22,8 +35,12 @@ fn check_number(number: u32) -> &'static str {
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
-    } else {
-        1
+    }
+    else if fizzish =="fuzz" {
+        return "bar"
+    }
+    else {
+        "baz"
     }
 }
 
@@ -31,14 +48,26 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
 // Determine if a given year is a leap year
 // Implement logic
 fn is_leap_year(year: i32) -> bool {
-    todo!()
+    if year % 4 != 0 {
+        return false;
+    } else if year % 100 != 0 {
+        return true;
+    } else if year % 400 != 0 {
+        return false;
+    }
+    true
 }
 
 // Exercise 5
 // Calculate the factorial of a number
 // Implement logic
 fn factorial(n: u32) -> u32 {
-    todo!()
+    if n == 0{
+        1
+    }
+    else{
+        n * factorial(n - 1)
+    }
 }
 
 // Exercise 6
@@ -46,9 +75,23 @@ fn factorial(n: u32) -> u32 {
 // Implement logic
 
 fn is_prime(n: u32) -> bool {
-    todo!()
-}
+    let mut count: i32 = 0;
+    if n < 2 {
+        return false;
+    }
 
+    // let sqrt = (n as f64).sqrt() as u32;
+
+    for i in 2..=n {
+        if n % i == 0 {
+            count += 1;
+        }
+    }
+    if count == 1{
+        return true;
+    }
+    return false;
+}
 
 // Don't mind this for now :)
 #[cfg(test)]
@@ -143,6 +186,5 @@ mod tests {
         assert_eq!(is_prime(10), false);
         assert_eq!(is_prime(15), false);
     }
-
 
 }
--- exercises/basic-of-rust/src/functions.rs
+++ exercises/basic-of-rust/src/functions.rs
@@ -1,7 +1,7 @@
 // Exercise 1
 // Fix all errors 
-fn sum(x, y: i32) {
-    x + y;
+fn sum(x : i32, y : i32) -> i32 {
+    x + y
 }
 
 //Exercise 2
@@ -11,7 +11,10 @@ fn sum(x, y: i32) {
 pub fn sum_one_to_n(n: u32) -> u32 {
     // your code for summing all digits from 1 to `n` (inclusive) should go
     // here (you can remove the sample return of `0`)
-    0
+    if n == 0 {
+        return 0
+    }
+    return sum_one_to_n(n - 1) + n
 }
 
 // Exercise 3
@@ -19,13 +22,25 @@ pub fn sum_one_to_n(n: u32) -> u32 {
 // Problem: Calculate the average of a list of numbers
 // Output: Average Number 
 fn calculate_average(numbers: &[f64]) -> f64 {
-    todo!()
+    if numbers.is_empty(){ return 0.0;}
+    let mut sum : f64 = 0.0;
+    for &num in numbers.iter(){
+        sum += num;
+    }
+    return sum / numbers.len() as f64;
 }
 
 // Exercise 4
 // Calculate the sum of all even numbers in a list
 fn sum_even_numbers(numbers: &[i32]) -> i32 {
-    todo!()
+    if numbers.is_empty(){ return 0;}
+    let mut sum : i32 = 0;
+    for &num in numbers.iter(){
+        if num % 2 == 0{
+            sum += num;
+        }
+    }
+    sum
 }
 
 
